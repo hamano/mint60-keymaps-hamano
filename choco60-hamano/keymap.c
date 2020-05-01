4e71b7fea1cd2245eed76b2999c8042bf4e0ac63
HAMANO Tsukasa <code@cuspy.org>
2020-05-02 05:17:54 +0900

    update choco60 keymap

--- choco60-hamano/keymap.c
+++ choco60-hamano/keymap.c
@@ -29,13 +29,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,     KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,     KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_FN,
-    KC_LALT, KC_LGUI, KC_SPC,  KC_LANG1,KC_SPC,  KC_KANA, KC_HENK
+    KC_LALT, KC_LGUI, KC_SPC,  KC_KANA, KC_SPC,  KC_HENK, KC_MHEN
   ),
   [_FN] = LAYOUT(
     RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
     KC_CAPS, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,   _______, KC_STOP,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN, KC_DOWN, _______, _______,
-    _______, _______, _______, KC_LANG2,_______, KC_RO,   KC_MHEN
+    _______, _______, _______, KC_APP , _______, KC_LANG1,KC_LANG2
   )
 };
